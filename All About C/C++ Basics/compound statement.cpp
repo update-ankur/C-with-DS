2de8bef8ca917c40e1db5b5fcc3d26c9378fe09a
Ankur Singh <37451861+update-ankur@users.noreply.github.com>
2020-07-02 17:45:21 +0530

    06

---
+++ All About C/C++ Basics/compound statement.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+  int x;
+  x=5;
+  x=x+10;
+  cout<<x<<endl;
+  x=5;
+  x+=10;
+  cout<<x<<endl;
+  x=5;
+  x=x*10;
+  cout<<x<<endl;
+  x=5;
+  x*=10;
+  cout<<x<<endl;
+
+  /* Let's try bigg cal */
+  x=2;
+  x=x*10*20*30;
+  cout<<x<<endl;
+  x=2;
+  x*=10;
+  x*=20;
+  x*=30;
+  cout<<x<<endl;
+
+  /* now x and a1 will have same value */
+
+
+  return 0;
+}
