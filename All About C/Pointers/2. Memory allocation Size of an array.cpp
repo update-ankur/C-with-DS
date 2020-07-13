cb7c47434f10ba8920234a835728f6a720afc73b
Ankur Singh <37451861+update-ankur@users.noreply.github.com>
2020-07-13 18:15:10 +0530

    Create 2. Memory allocation Size of an array.cpp

---
+++ All About C/Pointers/2. Memory allocation Size of an array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int size;
+  cout<<"Enter the size of an array: ";
+  cin>>size;
+  int p[size];
+
+  cout<<sizeof p<<endl;
+
+
+  return 0;
+}
