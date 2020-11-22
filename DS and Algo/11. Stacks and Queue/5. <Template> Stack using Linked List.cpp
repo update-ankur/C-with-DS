7d0be79e8a9bedb366064540293628ed16fc03ad
Ankur Singh <37451861+update-ankur@users.noreply.github.com>
2020-11-23 00:09:21 +0530

    Create 5. <Template> Stack using Linked List.cpp

---
+++ DS and Algo/11. Stacks and Queue/5. <Template> Stack using Linked List.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+class Node{
+public:
+    T data;
+    Node<T> *next;
+    Node(T data){
+        this->data=data;
+        next=NULL;
+    }
+};
+
+template <typename T>
+class StackUsingLL{
+    Node<T> *Head;
+    int size;
+public:
+    StackUsingLL(){
+        size=0;
+        Head=NULL;
+    }
+    int getsize(){
+        return size;
+    }
+    bool isempty(){
+        // also you can use size==0
+        return Head==NULL;
+    }
+    void push(T x){
+        Node<T> *p=new Node<T>(x);
+        p->next=Head;
+        Head=p;
+        size++;
+    }
+    T pop(){
+        int x=Head->data;
+        Node<T> *p=Head->next;
+        delete Head;
+        Head=p;
+        size--;
+        return x;
+    }
+    T Top(){
+        return Head->data;
+    }
+};
+
+int main(){
+    
+    StackUsingLL<char> c;
+    c.push(100);
+    c.push(102);
+    c.push(103);
+    cout<<c.getsize()<<endl;
+    cout<<c.pop()<<endl;
+    cout<<c.pop()<<endl;
+    cout<<c.isempty()<<endl;
+    cout<<c.Top()<<endl;
+    
+    return 0;
+}
