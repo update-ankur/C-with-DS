82b039edac73ffd6b2b1a83b22312d6744e8a6e2
Ankur Singh <37451861+update-ankur@users.noreply.github.com>
2020-11-26 20:56:33 +0530

    Create 12. Deleting tree using destructor.h

---
+++ DS and Algo/12. tree/12. Deleting tree using destructor.h
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode{
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+    TreeNode(T data){
+        this->data=data;
+    }
+    ~TreeNode(){
+        for(int i=0;i<children.size();i++){
+            delete children[i];
+        }
+    }
+};
+
+
+
