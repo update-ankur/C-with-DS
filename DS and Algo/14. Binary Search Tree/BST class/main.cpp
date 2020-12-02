6da1875caf4ec03c6adf34157a98051fabd226ff
Ankur Singh <37451861+update-ankur@users.noreply.github.com>
2020-12-02 18:58:57 +0530

    Create main.cpp

---
+++ DS and Algo/14. Binary Search Tree/BST class/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "BST.h"
+int main(){
+    
+    BST b;
+    b.insertData(2);
+    b.insertData(5);
+    b.insertData(1);
+    b.insertData(7);
+    b.insertData(3);
+    
+    b.printTree();
+    
+    
+    
+    return 0;
+}
